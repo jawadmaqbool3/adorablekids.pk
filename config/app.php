7e8b99004691ef1de92336158c890760be2e00e3
Jawad Maqbool <jawadmaqbool32@gmail.com>
2023-01-26 00:28:09 +0500

    changes

--- config/app.php
+++ config/app.php
@@ -57,8 +57,8 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
-    
-    'media_url' => env('MEDIA_ASSET_URL'),
+
+    'media_url' => env('MEDIA_ASSET_URL', 'http://pos.adorablekids.pk'),
 
     /*
     |--------------------------------------------------------------------------
